d4f2b932066361f74006dc5f96dc2ade7491d0dc
PogChampion <pogchampius@protonmail.com>
2019-08-30 19:17:54 +0200

    Updates

---
+++ precache-manifest.976cdcbcd9afb3f931befb7008b19a54.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "858e69d73e9d3a417b26540b57d1d9d3",
+    "url": "/pokedexlanguages2/index.html"
+  },
+  {
+    "revision": "319a5d60a6338760af44",
+    "url": "/pokedexlanguages2/static/css/2.023e65de.chunk.css"
+  },
+  {
+    "revision": "5d03cb5f45165b68d91a",
+    "url": "/pokedexlanguages2/static/css/main.74ee890a.chunk.css"
+  },
+  {
+    "revision": "319a5d60a6338760af44",
+    "url": "/pokedexlanguages2/static/js/2.b91c3a9e.chunk.js"
+  },
+  {
+    "revision": "5d03cb5f45165b68d91a",
+    "url": "/pokedexlanguages2/static/js/main.f93f0147.chunk.js"
+  },
+  {
+    "revision": "d8ab515d2add0366976b",
+    "url": "/pokedexlanguages2/static/js/runtime~main.4d49091a.js"
+  },
+  {
+    "revision": "f0109bea7791cb3071e74a0c4c50e9b3",
+    "url": "/pokedexlanguages2/static/media/budew_monochrome.f0109bea.png"
+  },
+  {
+    "revision": "7f8455b70734a87596991e1f24053edc",
+    "url": "/pokedexlanguages2/static/media/china.7f8455b7.svg"
+  },
+  {
+    "revision": "968aaa24eeffdd779e4fc9c27c5cb730",
+    "url": "/pokedexlanguages2/static/media/france.968aaa24.svg"
+  },
+  {
+    "revision": "d810f62137d64a0a58035c80489b7d54",
+    "url": "/pokedexlanguages2/static/media/germany.d810f621.svg"
+  },
+  {
+    "revision": "612e617f5d72975e63e53d793d757785",
+    "url": "/pokedexlanguages2/static/media/italy.612e617f.svg"
+  },
+  {
+    "revision": "1f905d23af144bc76e488f00d516de2e",
+    "url": "/pokedexlanguages2/static/media/japan.1f905d23.svg"
+  },
+  {
+    "revision": "8a067f19c6a32f781b8cb971a3214629",
+    "url": "/pokedexlanguages2/static/media/south-korea.8a067f19.svg"
+  },
+  {
+    "revision": "36938bbe277961386e7e309dcb30cbd8",
+    "url": "/pokedexlanguages2/static/media/spain.36938bbe.svg"
+  },
+  {
+    "revision": "e5564902e2642c5e6e2e98e68a7d41f5",
+    "url": "/pokedexlanguages2/static/media/united-kingdom.e5564902.svg"
+  }
+]);
